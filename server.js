5248759988796cbf3530fae3a24a0d803acf4ff6
ylorenzana <yloren38@lakers.mercyhurst.edu>
2017-08-12 13:25:59 -0400

    Fixed server.js to match unknown paths

--- server.js
+++ server.js
@@ -26,7 +26,11 @@ app.use('/stickies', stickiesRoute);
 
 app.get('/', (req, res) => {
   res.send();
-})
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(port, () => {
   console.log('Server running on port: ' + port);
